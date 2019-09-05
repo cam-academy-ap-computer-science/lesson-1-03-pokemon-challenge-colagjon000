9c67e1965f43433e0e9730c6ece20cd5cee2cf3d
Jonah Colagross <colagjon000@battlegroundps.org>
2019-09-05 15:46:16 -0700

    Added AnimalHello.java -- runs classroom assigned output.

---
+++ src/pokemon/AnimalHello.java
@@ -0,0 +1,11 @@
+package pokemon;
+
+public class AnimalHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("(\\__/)\n(o^.^)\nz(_(“)(“)");
+	}
+
+}
